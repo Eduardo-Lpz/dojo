ca0d2240e1da8844052de5a68b68865eca8b3d4a
Eduardo Lopez <jlopez@arkusnexus.com>
2024-08-01 18:01:54 -0700

    kata 4 v2

--- kata_4.rb
+++ kata_4.rb
@@ -1,11 +1,6 @@
 def word_pattern(pattern, string)
-  uniq_chars = pattern.chars.uniq
-  uniq_words = string.split.uniq
+  words = string.split
 
-  combined_hash = Hash[uniq_words.zip(uniq_chars)]
-
-  expected_pattern = ''
-  string.split.each { |word| expected_pattern << combined_hash[word].to_s }
-
-  pattern == expected_pattern
+  mapped_values = Hash[words.uniq.zip(pattern.chars.uniq)]
+  pattern == mapped_values.values_at(*words).join('')
 end
