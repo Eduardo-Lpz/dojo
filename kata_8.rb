3578a60212cc41d5348c789dad25757f4d0f790b
Eduardo Lopez <jlopez@arkusnexus.com>
2024-09-06 17:40:18 -0700

    kata 8

--- kata_8.rb
+++ kata_8.rb
@@ -1,4 +1,4 @@
-def kata(a1: [], a2: [])
+def a1_thick_and_hearty(a1: [], a2: [])
   lengths = [a1.size, a2.size]
 
   common_values = a1.intersection(a2)
@@ -16,3 +16,20 @@ end
 # # a1: [1, 2, 3, 5, 10, 15],
 # # a2: [1, 2, 3, 4, 5, 6, 10, 12, 15, 16]
 # )
+
+# from itertools import permutations
+
+# def a1_thick_and_hearty(a1, a2):
+#     lengths = [len(a1), len(a2)]
+
+#     common_values = set(a1).intersection(a2)
+
+#     if not common_values:
+#         return set()
+
+#     result = set()
+#     for a, b in permutations(common_values, 2):
+#         if (a + b) in lengths or abs(a - b) in lengths:
+#             result.update([a, b])
+
+#     return result
