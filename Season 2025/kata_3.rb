627978a86126129fd421b257c456d557f6dc0bee
Eduardo Lopez <eduardolpz.dev@gmail.com>
2025-01-30 11:01:46 -0800

    kata_2025-3-v2

--- Season 2025/kata_3.rb
+++ Season 2025/kata_3.rb
@@ -8,3 +8,23 @@ end
 #     .map { _2.divmod(2).first }
 #     .sum
 # end
+
+def number_of_pairs(gloves)
+  mapped_gloves = {}
+
+  gloves.each do
+    if mapped_gloves[_1]
+      mapped_gloves[_1] += 1
+    else
+      mapped_gloves[_1] = 1
+    end
+  end
+
+  pairs = 0
+
+  mapped_gloves.each do
+    pairs += _2 / 2
+  end
+
+  pairs
+end
