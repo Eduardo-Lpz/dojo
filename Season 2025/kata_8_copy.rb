c96ddbfdffa68fbbb9f30bd125fad45b0e5ffa7b
Eduardo Lopez <eduardolpz.dev@gmail.com>
2025-03-05 15:24:11 -0800

    test

--- Season 2025/kata_8_copy.rb
+++ Season 2025/kata_8_copy.rb
@@ -1,13 +1,13 @@
-require 'prime'
+# require 'prime'
 
-def consec_kprimes(k, arr)
-  count = 0
+# def consec_kprimes(k, arr)
+#   count = 0
 
-  results = arr.map do |val|
-    val.prime_division.sum { _1.last }
-  end
+#   results = arr.map do |val|
+#     val.prime_division.sum { _1.last }
+#   end
 
-  results.each_cons(2) { count += 1 if _1.first == k && _1.last == k }
+#   results.each_cons(2) { count += 1 if _1.first == k && _1.last == k }
 
-  count
-end
+#   count
+# end
